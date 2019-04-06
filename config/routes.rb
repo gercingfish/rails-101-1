a6bbc5b3df5c63177f64dc2ddacacabf867db43f
yubohan <gercingfish@gmail.com>
2019-04-06 17:14:52 +0800

    replace with groups#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :groups
-  root 'welcome#index'
+  root 'groups#index'
+  #root 'welcome#index'
 end
